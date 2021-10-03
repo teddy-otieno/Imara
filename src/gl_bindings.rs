7a5876fb69786c868698bf38a630a0870a061218
Teddy Juma <tedd.juma@outlook.com>
2021-10-03 05:46:38 +0300

    Added error logging and debuging

--- src/gl_bindings.rs
+++ src/gl_bindings.rs
@@ -1,5 +1,7 @@
-use glfw::{Context, WindowEvent};
 use std::sync::mpsc::Receiver;
+use std::ffi::{c_void, CString};
+use std::convert::TryInto;
+use glfw::{Context, WindowEvent};
 
 pub struct Display {
     pub glfw: glfw::Glfw,
@@ -36,7 +38,12 @@ pub fn init_gl_window_context(initial_size: (u32, u32), window_name: &str) -> Di
     gl::load_with(|s| window.get_proc_address(s) as *const _);
     gl::Viewport::load_with(|s| window.get_proc_address(s));
 
-    unsafe { gl::Viewport(0, 0, initial_size.0 as i32, initial_size.1 as i32) };
+    unsafe { 
+        gl::Viewport(0, 0, initial_size.0 as i32, initial_size.1 as i32);
+        gl::Enable(gl::DEBUG_OUTPUT);
+        gl::DebugMessageCallback(Some(message_callback), 0 as *const c_void);
+    };
+
 
     glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
 
@@ -46,3 +53,30 @@ pub fn init_gl_window_context(initial_size: (u32, u32), window_name: &str) -> Di
         events_receiver: events,
     }
 }
+
+
+extern "system" fn message_callback(
+    source: gl::types::GLenum, 
+    e_type: gl::types::GLenum, 
+    id: gl::types::GLuint,
+    severity: gl::types::GLenum,
+    length: gl::types::GLsizei,
+    message: *const gl::types::GLchar,
+    user_param: *mut c_void,
+) {
+
+    let mut message_buffer = Vec::with_capacity(length.try_into().unwrap());
+
+    unsafe {
+        for i in 0..length { 
+            message_buffer.push(*message.offset(i.try_into().unwrap())) 
+        }
+
+        let message_bytes: Vec<u8> = message_buffer.into_iter().map(|x| x as u8).collect();
+        let c_string = CString::from_vec_unchecked(message_bytes);
+
+        eprintln!("GL CALLBACK: type = {}, severity = {}, {:?}", e_type, severity, c_string);
+    }
+
+}
+
